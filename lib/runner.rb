cd585fb57f4e734bf293ae8928d53a36efcd73fe
amcolling <amcolling@gmail.com>
2018-05-04 15:01:14 -0600

    created runner

---
+++ lib/runner.rb
@@ -0,0 +1,9 @@
+require './lib/credit_check'
+
+
+credit_check = CreditCheck.new
+credit_check.convert_to_array
+credit_check.reverse_array
+credit_check.doubled_every_other
+credit_check.add_digits_over_ten
+credit_check.module_of_ten
