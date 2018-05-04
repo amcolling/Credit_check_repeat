1bebc1d662d0e4ba4f74c22fa29bd6fb4b0373f1
amcolling <amcolling@gmail.com>
2018-05-04 09:06:14 -0600

    created tests

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,8 +1,27 @@
+require 'pry'
 class CreditCheck
   attr_reader :cc_number
 
   def initialize
     @cc_number = "4929735477250543"
+  end
+
+  def convert_to_array
+    @cc_number.chars
+  end
+
+  def reverse_array
+    @cc_number.to_i.digits
+  end
+
+  def doubled
+    reverse_array.map.with_index do |digit, index|
+      if index.odd? == true
+        digit * 2
+      else
+        digit
+      end
+    end
   end
 
 
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -12,7 +12,22 @@ class CreditCheckTest < Minitest::Test
   def test_credit_card_number
     credit_check = CreditCheck.new
     assert_equal "4929735477250543", credit_check.cc_number
-  end 
+  end
+
+  def test_convert_to_array
+    credit_check = CreditCheck.new
+    assert_equal ["4", "9", "2", "9", "7", "3", "5", "4", "7", "7", "2", "5", "0", "5", "4", "3"], credit_check.convert_to_array
+  end
+
+  def test_reverse_array
+    credit_check = CreditCheck.new
+    assert_equal [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4], credit_check.reverse_array
+  end
+
+  def test_doubled
+    credit_check = CreditCheck.new
+    assert_equal [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8], credit_check.doubled
+  end
 
 
 
